d97d4ebb33863d4acd9f9d8d1a033186377de41b
DeliKhan <deluckshanmurugamoorthy@gmail.com>
2024-01-09 21:01:14 -0500

    Feat: accept multiple course inclusions/exclusions

--- app/coursesdropdown.tsx
+++ app/coursesdropdown.tsx
@@ -10,6 +10,10 @@ function Course({cour,}: {cour : Promise<string[]>;}) {
     const [inputClass, setInputClass] = useState('');
     const [inputValue2, setinputValue2] = useState('');
     const [options2, setoptions2] = useState<string[]>([]);
+    const [dropdownVisible1, setDropdownVisible1] = useState(false);
+    const [dropdownVisible2, setDropdownVisible2] = useState(false);
+    const [selected1, setSelected1] = useState<string[]>([]);
+    const [selected2, setSelected2] = useState<string[]>([]);
 
     useEffect(() => {
         //Right before I call the updateCourse function, I make the user's pointer a wait pointer
@@ -31,22 +35,50 @@ function Course({cour,}: {cour : Promise<string[]>;}) {
     
     return (
         //When the user types in the input field, this changes its value, which the useEffect above will get all courses to create a dropdown
+        //The point of onMouseDown is to prevent the input field from losing focus when the user clicks on the dropdown
+        //onFocus={e => e.target.nextElementSibling?.classList.remove("hidden")} onBlur={e => e.target.nextElementSibling?.classList.add("hidden")}
         <>
             <div>
                 <label htmlFor="Exclude" className="block text-sm font-medium leading-6 text-gray-900">Exclude These Courses</label>
                 <div className="mt-3">
-                    <input type="text" aria-placeholder="Type Course..." id="Exclude" value={inputValue1} onFocus={e => e.target.nextElementSibling?.classList.remove("hidden")} onBlur={e => e.target.nextElementSibling?.classList.add("hidden")} onChange={e => setinputValue1(e.target.value)} className={"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "+inputClass}></input>
-                    <div id="dropdown1" className="overflow-y-auto max-h-52 rounded-md border-2 border-slate-400">
-                        {options1.map(option => <a onClick={() => setinputValue1(option)} className="text-gray-700 block rounded-sm px-4 py-2 border-b-2 border-slate-300 hover:bg-slate-300 cursor-pointer text-sm">{option}</a>)}
+                    <input type="text" aria-placeholder="Type Course..." id="Exclude" value={inputValue1} onFocus={() => setDropdownVisible1(true)} onBlur={() => setDropdownVisible1(false)} onChange={e => setinputValue1(e.target.value)} className={"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "+inputClass}></input>
+                    <div id="dropdown1" className={`overflow-y-auto max-h-52 rounded-md border-2 border-slate-400 ${dropdownVisible1 ? '' : 'hidden'}`}>
+                        {options1.map(option => <a onMouseDown={e => e.preventDefault()} onClick={() => {setinputValue1(""); setDropdownVisible1(true);setSelected1(prevOptions => prevOptions.includes(option) ? prevOptions : [...prevOptions, option]);}} className="text-gray-700 block rounded-sm px-4 py-2 border-b-2 border-slate-300 hover:bg-slate-300 cursor-pointer text-sm">{option}</a>)}
+                    </div>
+                    <div className="flex flex-wrap gap-y-2 gap-x-1 pt-2">
+                        {selected1.map((option : string) => 
+                            <div className="flex grow-0 w-fit content-center gap-2 bg-amber-500 font-semibold rounded-md px-3 py-2">
+                                {option.split(" ").slice(0,2).join(" ") + "..."}
+                                <button type="button" onMouseDown={e => e.preventDefault()} onClick={() => setSelected1(prevOptions => prevOptions.filter(prevOption => prevOption !== option))} className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                    <span className="sr-only">Close menu</span>
+                                    <svg className="h-2.5 w-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
             <div>
                 <label htmlFor="Include" className="block text-sm font-medium leading-6 text-gray-900">Include These Courses</label>
                 <div className="mt-3">
-                    <input type="text" aria-placeholder="Type Course..." id="Include" value={inputValue2} onFocus={e => e.target.nextElementSibling?.classList.remove("hidden")} onBlur={e => e.target.nextElementSibling?.classList.add("hidden")} onChange={e => setinputValue2(e.target.value)} className={"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "+inputClass}></input>
-                    <div id="dropdown2" className="overflow-y-auto max-h-52 rounded-md border-2 border-slate-400">
-                        {options2.map(option => <a onClick={() => setinputValue2(option)} className="text-gray-700 block rounded-sm px-4 py-2 border-b-2 border-slate-300 hover:bg-slate-300 cursor-pointer text-sm">{option}</a>)}
+                    <input type="text" aria-placeholder="Type Course..." id="Include" value={inputValue2} onFocus={() => setDropdownVisible2(true)} onBlur={() => setDropdownVisible2(false)} onChange={e => setinputValue2(e.target.value)} className={"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "+inputClass}></input>
+                    <div id="dropdown2" className={`overflow-y-auto max-h-52 rounded-md border-2 border-slate-400 ${dropdownVisible2 ? '' : 'hidden'}`}>
+                        {options2.map(option => <a onMouseDown={e => e.preventDefault()} onClick={() => {setinputValue2("");setDropdownVisible2(true);setSelected2(prevOptions2 => prevOptions2.includes(option) ? prevOptions2 : [...prevOptions2, option]);}} className="text-gray-700 block rounded-sm px-4 py-2 border-b-2 border-slate-300 hover:bg-slate-300 cursor-pointer text-sm">{option}</a>)}
+                    </div>
+                    <div className="flex flex-wrap gap-y-2 gap-x-1 pt-2">
+                        {selected2.map((option : string) => 
+                            <div className="flex grow-0 w-fit content-center gap-2 bg-amber-500 font-semibold rounded-md px-3 py-2">
+                                {option.split(" ").slice(0,2).join(" ") + "..."}
+                                <button type="button" onMouseDown={e => e.preventDefault()} onClick={() => setSelected2(prevOptions2 => prevOptions2.filter(prevOption2 => prevOption2 !== option))} className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                    <span className="sr-only">Close menu</span>
+                                    <svg className="h-2.5 w-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
